64520167b512ce2080e55a31f3b8977796445e09
= <smadhu1803@gmail.com>
2022-12-26 22:50:55 -0800

    Send HTTP responses from server

--- database.js
+++ database.js
@@ -2,43 +2,59 @@ const firebase = require('firebase/app');
 const { getDatabase, set, ref, remove, onValue} = require('firebase/database');
 require('firebase/database');
 
-const firebaseConfig = require("./config.json")
+const firebaseConfig = require("./firebase-config.json")
 
 const app = firebase.initializeApp(firebaseConfig);
 const database = getDatabase(app);
 
-//add to realtime database using reference 
-async function addRecipeToBook(user, recipe, database)
+//add to realtime database using reference with user id
+async function addRecipeToBook(user, recipe)
 {
     return new Promise ((resolve) => {
         set(ref(database, 'users/' + user + '/recipes/' + recipe), {
             recipeName: recipe
           })
         .then (() => {
-            console.log("done!");
             resolve();
+        })
+        .catch ((message) =>
+        {
+            reject(message);
         })
     });
 }
 
 //remove from realtime database using reference 
-async function removeRecipeFromBook(user, recipe, database)
+async function removeRecipeFromBook(user, recipe)
 {
-    return new Promise ((resolve) => {
+    return new Promise ((resolve, reject) => {
         remove(ref(database, 'users/' + user + '/recipes/' + recipe))
         .then (() => {
-            console.log("done!");
             resolve();
+        })
+        .catch ((message) => {
+            reject(message);
         })
     });
 }
 
 //listen for changes in this user's recipe list' and retrieve list
-async function readRecipes (user, database)
+function readRecipes (user)
 {
     const userRef = ref (database, 'users/' + user);
-    onValue (userRef, (snapshot) => {
-        console.log (snapshot.val());
+    return new Promise ((resolve, reject) => {
+        onValue (userRef, (snapshot) => {
+            if (snapshot.exists())
+            {
+                const data = JSON.stringify(snapshot.val());
+                console.log(data);
+                resolve(data);
+            }
+            else
+            {
+                reject("Data at this reference doesn't exist");
+            }
+        })
     });
 }
 module.exports = {database, addRecipeToBook, removeRecipeFromBook, readRecipes};
