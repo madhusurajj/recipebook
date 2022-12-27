65009828c652d8a66b0633a58350eb12a6a21448
= <smadhu1803@gmail.com>
2022-12-26 22:50:55 -0800

    Send HTTP responses from server

--- server.js
+++ server.js
@@ -1,28 +1,54 @@
-const {readRecipes} = require ("./database.js");
+const {readRecipes, addRecipeToBook, removeRecipeFromBook} = require ("./database.js");
 
 const express = require('express');
-
-const app = express();
 const cors = require ('cors');
 
-//middleware to allow frontend requests from cross-origin domain to this server
+const app = express();
+
+//middleware to allow browser requests from cross-origin domains
 app.use(cors({
     origin: "localhost:4200"
 })); 
 
-//HTTP backend response to get all recipes created by a user
+//HTTP GET response to get all recipes created by a user
 app.get('/users/:userID', cors(), (req, res) => {
     const userID = req.params.userID;
     readRecipes(userID)
     .then ((data) =>
     {
-        res.json(data);
+        res.status(200).json(data);
     })
     .catch((reason) => 
     {
-        res.status(400);
+        res.status(400).send(reason);
     }
     );
+});
+
+//HTTP POST response to create new recipes
+app.post('/users/:userID/:recipeName', cors(), (req, res) => {
+    addRecipeToBook (req.params.userID, req.params.recipeName)
+    .then (() =>
+    {
+        res.status(201).send({message: "Succcessfully added recipe"});
+    })
+    .catch (() => 
+    {
+        res.status(400).send({message: "Could not add recipe"});; 
+    })
+});
+
+//HTTP DELETE response to create new recipes
+app.delete('/users/:userID/:recipeName', cors(), (req, res) => {
+    removeRecipeFromBook (req.params.userID, req.params.recipeName)
+    .then (() =>
+    {
+        res.status(204).send({message: "Succcessfully deleted recipe"});
+    })
+    .catch ((message) => 
+    {
+        res.status(400).send(message);; 
+    })
 });
 
 app.listen(8000, () => {
