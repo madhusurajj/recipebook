f1a04eba4846aba8788e7131a2d2dc8140fc81b8
= <smadhu1803@gmail.com>
2023-01-04 23:53:37 -0800

    Switch from Realtime Database to Cloud Firestore to supportmore complex queries. Update all database functions

--- database.js
+++ database.js
@@ -1,22 +1,23 @@
 const firebase = require('firebase/app');
-const { getDatabase, set, ref, remove, onValue, startAt, endAt, push, equalTo} = require('firebase/database');
-const {query, orderByChild} = require('firebase/database');
+const { getFirestore, collection, addDoc, query, doc, deleteDoc, getDocs, where} = require("firebase/firestore");
 
 const firebaseConfig = require("./firebase-config.json")
 
 const app = firebase.initializeApp(firebaseConfig);
-const database = getDatabase(app);
+const database = getFirestore(app);
 
 const {filterForIngredients} = require("./util.js");
 
 //add to realtime database using reference with user id
 //add/update ingredients with optional param
-async function addRecipeToBook(user, recipeName, ingredients = null)
+async function addRecipeToBook(user, name, ingredient = null)
 {
     return new Promise ((resolve) => {
-        set(ref (database, 'users/' + user + '/recipes/' + recipeName), 
-            ingredients
-        )
+        const recipeDocRef = addDoc(collection(database, "recipes"), 
+        {
+            userID: user,
+            recipeName: name
+        })
         .then (() => {
             resolve();
         })
@@ -27,35 +28,40 @@ async function addRecipeToBook(user, recipeName, ingredients = null)
     });
 }
 
-//remove from realtime database using reference 
+//delete recipes with this name from firestore
 async function removeRecipeFromBook(user, recipe)
 {
-    return new Promise ((resolve, reject) => {
-        remove(ref(database, 'users/' + user + '/recipes/' + recipe))
-        .then (() => {
+    return new Promise (async (resolve, reject) =>  {
+        const q = query(collection(database, "recipes"), where("recipeName", "==", recipe));
+        const snapshotToDelete = await getDocs(q);
+        try
+        {
+            snapshotToDelete.forEach(async (document) => {
+                await deleteDoc(doc(database, "recipes", document.id));
+            });
             resolve();
-        })
-        .catch ((message) => {
-            reject(message);
-        })
+        }
+        finally 
+        {
+            reject({message: "Could not delete" + recipe});
+        }
     });
 }
 
-//listen for changes in this user's recipe list' and retrieve list
-//prepTime: optional parameter allows users to filter by length of preparation
-function readRecipes (user, prepTime = null)
+//listen for changes in this user's recipe list' and retrieve
+//access all recipe documents in the collection, querying by userID
+function readRecipes (user)
 {
-    return new Promise ((resolve, reject) => {
-         onValue (query(ref(database, 'users/' + user + '/recipes'), orderByChild("prep-time"),equalTo(prepTime)), (snapshot) => {
-            if (snapshot.exists())
-            {
-                resolve(snapshot.val());
-            }
-            else
-            {
-                reject("Data at this reference doesn't exist");
-            }
+    let recipesAsJson = {};
+    return new Promise (async (resolve, reject) => {
+        const q = query(collection(database, "recipes"), where("userID", "==", user));
+        const querySnapshot = await getDocs(q);
+        let enumeration = 0;
+        querySnapshot.forEach((doc) => {
+            recipesAsJson[enumeration] = doc.data();
+            enumeration++;
         });
+        resolve(recipesAsJson);
     });
 }
 module.exports = {database, addRecipeToBook, removeRecipeFromBook, readRecipes};
